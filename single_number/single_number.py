16ad1df278e7e4162246c8ce2ff13ba2ac8be7f1
Teagan Keith <teagankeith@outlook.com>
2020-09-17 08:01:13 -0400

    Finished Single Numbers

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -5,7 +5,15 @@ Returns: an integer
 def single_number(arr):
     # Your code here
 
-    pass
+    # We have to iterate through the arr to find the number that only occurs once,
+    # once we find that number, we just return it
+
+    # We can just the .count() method to check how many times the number we are on
+    # occurs in the array we are iterating through
+    for num in arr:
+        if arr.count(num) == 1:
+            correctnum = num
+    return correctnum
 
 
 if __name__ == '__main__':
